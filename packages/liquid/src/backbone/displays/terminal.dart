053b041af244999bffa364cbcc9fa4b5485da603
Sage Martin <sagethemartin@gmail.com>
2015-10-10 15:15:23 -0500

    Starting Implementation of textColor, textBack

--- packages/liquid/src/backbone/displays/terminal.dart
+++ packages/liquid/src/backbone/displays/terminal.dart
@@ -6,9 +6,18 @@ abstract class Terminal {
   int get width;
   int get height;
 
+  void clear() {
+    for (var y = 0; y < height; y++) {
+      for (var x = 0; x < width; x++) {
+        drawToken(x, y, Char.DERP);
+      }
+    }
+  }
+
+  void render();
 
 
-
+  void drawToken(int x, int y, Char chars);
 
 
 }
@@ -24,12 +33,7 @@ class Font {
 }
 
 
-abstract class herp extends Terminal {
-  void render();
-
-}
-
-class derp extends herp {
+class derp extends Terminal {
   final Font _font;
   final htm.CanvasElement _canvas;
   htm.CanvasRenderingContext2D _contex;
@@ -51,12 +55,15 @@ class derp extends herp {
 
   }
 
+  /**
+   * Create A new Canvas
+   *
+   */
   derp(int width, int height, this._canvas, this._font) {
     _contex = _canvas.context2D;
     _canvas.width = width * _font.charWidth;
     _canvas.height = height * _font.charHeight;
     _canvas.style.width = '${_canvas.width}px';
     _canvas.style.height ='${_canvas.height}px';
-
   }
 }
--- web/packages/liquid/src/backbone/displays/token.dart
+++ web/packages/liquid/src/backbone/displays/token.dart
@@ -6,7 +6,7 @@ const MENUCHARSCHARS = const [
   r"\ .  /   \  . /                          \  . /        \ . |         ",
   r" | .|     |. |                            |. |          | .|     ",
   r" |. |     | .|   ___________ _____  ____  | .|   ______ |. |        ",
-  r" |:::     |::|  /:::::::::::|\:::|  \:::| |::|  /::::::\|::|            ",
+  r" |::|     |::|  /:::::::::::|\:::|  \:::| |::|  /::::::\|::|            ",
   r" |xx|     |xx|  |xx|     |xx| |xx|   |xx| |xx| /xx/     |xx|             ",
   r" |xx|     |xx|  |xx|     |XX| |xx|   |xx| |xx| |xx|     |xx|        ",
   r" |XX|     |XX|  |XX|     |XX| |XX\___|XX| |XX| |XXX\____|XX|          ",
@@ -37,18 +37,25 @@ const MENUCHARSCHARS = const [
 class Char {
 
   final int char;
-  static const DERP = const Char.fromCharCode(0x0020);
+  final String textColor;
+  final String textBack;
+  static const DERP = const Char.fromCharCode(CharCode.SPACE);
 
-  Char(String char): char = char.codeUnits[0];
+  Char(String char, [Color textColor, Color textBack]): char = char.codeUnits[0] , textColor = textColor != null ? textColor : Color.WHITE, textBack = textBack != null ? textBack : Color.BLACK;
 
-  const Char.fromCharCode(this.char);
+  const Char.fromCharCode(this.char, [Color textColor, Color textBack]) : textColor = textColor != null ? textColor : Color.WHITE, textBack = textBack != null ? textBack : Color.BLACK ;
+
+  factory Char.derp(charOrCharCode, [Color fore, Color back]) {
+    if (charOrCharCode is String) return new Char(charOrCharCode, fore, back);
+    return new Char.fromCharCode(charOrCharCode, fore, back);
+  }
 }
 
 
 class Color {
   final String cccol;
-  static String BLACK   = '#000';
-  static String WHITE        = '#ffffff';
+  static const String BLACK   = '#000';
+  static const String WHITE        = '#ffffff';
 
   static String LIGHT_GRAY   = 'rgb(192, 192, 192)';
   static String GRAY         = 'rgb(128, 128, 128';
@@ -91,4 +98,30 @@ class Color {
   static String DARK_BROWN   = 'rgb100, 64, 32';
 
   Color(this.cccol);
+}
+
+
+/// Unicode code points for various special characters that also exist on
+/// [code page 437][font].
+///
+/// [font]: http://en.wikipedia.org/wiki/Code_page_437
+// Note: If you add stuff to this, make sure to add an appropriate mapping in
+// canvas_terminal.dart.
+class CharCode {
+  static const SPACE = 0x0020;
+  static const ASTERISK = 0x002a;
+  static const MIDDLE_DOT = 0x00b7;
+  static const BULLET = 0x2022;
+  static const BULLET_OPERATOR = 0x2219;
+  static const LEFT_RIGHT_ARROW = 0x2194;
+  static const UP_DOWN_ARROW = 0x2195;
+  static const SOLID = 0x2588;
+  static const HALF_LEFT = 0x258c;
+  static const BLACK_UP_POINTING_TRIANGLE = 0x25b2;
+  static const BLACK_SPADE_SUIT = 0x2660;
+  static const BLACK_CLUB_SUIT = 0x2663;
+  static const BOX_DRAWINGS_LIGHT_VERTICAL = 0x2502;
+  static const TRIPLE_BAR = 0x2261;
+  static const PI = 0x03C0;
+  static const BLACK_HEART_SUIT = 0x2665;
 }
